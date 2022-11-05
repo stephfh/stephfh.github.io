d9ae40da26c8d1b57c5d768a43242f62ded730ff
Stephanie Handsteiner <st.handsteiner@gmail.com>
2022-11-05 22:10:37 +0100

    task: add build action

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-	// your configuration options here...
-	// https://docs.astro.build/en/reference/configuration-reference/
+	site: 'https://stephfh.dev'
 })
